69ad2dd24effa558bccbed2b6ed305404f10d7be
jihyung <coolishh@naver.com>
2025-06-04 18:25:56 +0900

    desktop css added

--- js/script.js
+++ js/script.js
@@ -49,3 +49,18 @@ parentDivs4.forEach(parentDiv4 => {
     parentDiv4.prepend(childDiv1);
     parentDiv4.appendChild(childDiv2);
 });
+
+const parentDivs5 = document.querySelectorAll('.holes');
+parentDivs5.forEach(parentDiv5 => {
+    const childDiv1 = document.createElement('div');
+    const childDiv2 = document.createElement('div');
+    const childDiv3 = document.createElement('div');
+
+    childDiv1.className = 'hole1';
+    childDiv2.className = 'hole2';
+    childDiv3.className = 'hole3';
+
+    parentDiv5.appendChild(childDiv1);
+    parentDiv5.appendChild(childDiv2);
+    parentDiv5.appendChild(childDiv3);
+});
