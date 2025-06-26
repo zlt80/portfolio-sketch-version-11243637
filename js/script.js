55fc1f022b7ff470003f059a9c66d357035402cd
jihyung <coolishh@naver.com>
2025-06-26 11:33:08 +0900

    work experiences details part countries - css test

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-const parentDivs2 = document.querySelectorAll('.website');
-parentDivs2.forEach(parentDiv2 => {
-    const childDiv = document.createElement('div');
-    childDiv.className = 'circles';
-    parentDiv2.prepend(childDiv);
-});
-
-const parentDivs3 = document.querySelectorAll('.circles');
-parentDivs3.forEach(parentDiv3 => {
-
-    const childDiv1 = document.createElement('div');
-    const childDiv2 = document.createElement('div');
-    const childDiv3 = document.createElement('div');
-
-    childDiv1.className = 'circle c1';
-    childDiv2.className = 'circle c2';
-    childDiv3.className = 'circle c3';
-
-    parentDiv3.appendChild(childDiv1);
-    parentDiv3.appendChild(childDiv2);
-    parentDiv3.appendChild(childDiv3);
-});
-
 const parentDivs4 = document.querySelectorAll('.phone-frame');
 parentDivs4.forEach(parentDiv4 => {
     const childDiv1 = document.createElement('div');
